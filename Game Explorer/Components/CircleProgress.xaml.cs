3b72b255535bccd0b1097ea9d17e0dcc6a22e5ab
isaac <isaacramosflores454@gmail.com>
2024-07-25 14:56:07 -0600

    feat: Added some components

---
+++ Game Explorer/Components/CircleProgress.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Timers;
+using System.Windows;
+using Game_Explorer.Class;
+
+namespace Game_Explorer.Components
+{
+    /// <summary>
+    /// Interaction logic for CircleProgress.xaml
+    /// </summary>
+    public sealed partial class CircleProgress : INotifyPropertyChanged
+    {
+        private double _value1;
+        private double _value2;
+
+        public double Value1
+        {
+            get => _value1;
+            set
+            {
+                _value1 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double Value2
+        {
+            get => _value2;
+            set
+            {
+                _value2 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public CircleProgress()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void InitializeTimer()
+        {
+            var timer1 = new Timer(1200);
+            timer1.Elapsed += (sender, args) => UpdateInformation();
+            timer1.Start();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void UpdateInformation()
+        {
+            Value1 = SystemInfo.GetCpuUsage();
+
+            Value2 = SystemInfo.GetUsedRamAsPercentage();
+            Ram.Text = $"{SystemInfo.GetUsedRam()}Gib / {SystemInfo.GetTotalRam()}Gib";
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            UpdateInformation();
+            Ram.Text = $"{SystemInfo.GetUsedRam()}Gib / {SystemInfo.GetTotalRam()}Gib";
+            CpuModel.Text = SystemInfo.GetCpuModel();
+            InitializeTimer();
+        }
+    }
+}
---
+++ Game Explorer/Components/Game.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using Game_Explorer.Class;
+
+namespace Game_Explorer.Components
+{
+    /// <summary>
+    /// Interaction logic for Game.xaml
+    /// </summary>
+    public partial class Game
+    {
+        private readonly string _gamePath;
+
+        public Game(GameInfo game)
+        {
+            InitializeComponent();
+
+            _gamePath = game.ExecutablePath;
+            Title.Text = game.Name;
+            if (game.LogoPath != null) Image.ImageSource = new BitmapImage(new Uri(game.LogoPath));
+        }
+
+        private void StartGame_Click(object sender, RoutedEventArgs e)
+        {
+            if (_gamePath == null)
+            {
+                MessageBox.Show("Game path not found", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                Process.Start(_gamePath);
+            }
+        }
+    }
+}
