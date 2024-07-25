9f6b83db2fb4abd03b481b41d3db436fff9157cd
isaac <isaacramosflores454@gmail.com>
2024-07-25 14:55:33 -0600

    feat: Added Sys info & game detector

---
+++ Game Explorer/Class/GameDetector.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Game_Explorer.Class
+{
+    public class GameInfo
+    {
+        public string Name { get; set; }
+        public string LogoPath { get; set; }
+        public string ExecutablePath { get; set; }
+    }
+
+    public abstract class GameDetector
+    {
+        private static readonly string[] GamesPaths =
+        {
+            @"C:\Program Files (x86)\Steam\steamapps\common",
+            @"C:\Program Files (x86)\Ubisoft\Ubisoft Game Launcher\games",
+            @"C:\XboxGames"
+        };
+
+        public static List<GameInfo> DetectInstalledGamesWithLogos()
+        {
+            var gamesList = new List<GameInfo>();
+
+            foreach (var directory in GamesPaths)
+            {
+                gamesList.AddRange(DetectGamesInDirectory(directory));
+            }
+
+            return gamesList;
+        }
+
+        private static List<GameInfo> DetectGamesInDirectory(string directoryPath)
+        {
+            var gamesList = new List<GameInfo>();
+
+            if (!Directory.Exists(directoryPath)) return gamesList;
+            var gameDirectories = Directory.GetDirectories(directoryPath);
+            gamesList.AddRange(from directory in gameDirectories
+                let gameName = new DirectoryInfo(directory).Name
+                let logoPath = GetGameLogoPath(directory, gameName)
+                let executablePath = FindExecutable(directory, gameName)
+                select new GameInfo { Name = gameName, LogoPath = logoPath, ExecutablePath = executablePath });
+
+            return gamesList;
+        }
+
+        private static string FindExecutable(string gamePath, string gameName)
+        {
+            var exeFiles = Directory.GetFiles(gamePath, "*.exe", SearchOption.AllDirectories);
+            var exactMatch = exeFiles.FirstOrDefault(file =>
+                Path.GetFileNameWithoutExtension(file).Equals(gameName, StringComparison.OrdinalIgnoreCase));
+
+            if (exactMatch != null && File.Exists(exactMatch))
+            {
+                return exactMatch;
+            }
+
+            var bestMatch = exeFiles.FirstOrDefault(file =>
+                Path.GetFileNameWithoutExtension(file).ToLower().Contains(gameName.ToLower()));
+
+            return bestMatch != null && File.Exists(bestMatch) ? bestMatch : null;
+        }
+
+        private static string GetGameLogoPath(string gamePath, string gameName)
+        {
+            var imageExtensions = new[] { ".png", ".jpg", ".jpeg", ".ico" };
+            var logoFiles = Directory.GetFiles(gamePath, "*.*", SearchOption.AllDirectories)
+                .Where(file => imageExtensions.Contains(Path.GetExtension(file).ToLower()));
+
+            var enumerable = logoFiles as string[] ?? logoFiles.ToArray();
+            var bestMatch = enumerable.FirstOrDefault(file =>
+                Path.GetFileNameWithoutExtension(file).ToLower().Contains(gameName.ToLower()));
+
+            return bestMatch ?? enumerable.FirstOrDefault();
+        }
+    }
+}
---
+++ Game Explorer/Class/SystemInfo.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Management;
+
+namespace Game_Explorer.Class
+{
+    public static class SystemInfo
+    {
+        public static int GetCpuUsage()
+        {
+            using (var searcher =
+                   new ManagementObjectSearcher(
+                       "SELECT * FROM Win32_PerfFormattedData_PerfOS_Processor WHERE Name='_Total'"))
+            {
+                foreach (var o in searcher.Get())
+                {
+                    var obj = (ManagementObject)o;
+                    if (obj["PercentProcessorTime"] != null)
+                    {
+                        return (int)float.Parse(obj["PercentProcessorTime"].ToString());
+                    }
+                }
+            }
+
+            return 0;
+        }
+
+        public static string GetCpuModel()
+        {
+            var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_Processor");
+            foreach (var o in searcher.Get())
+            {
+                var obj = (ManagementObject)o;
+                return obj["Name"].ToString();
+            }
+
+            return "N/A";
+        }
+
+        public static int GetTotalRam()
+        {
+            long totalRamBytes = 0;
+            var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_ComputerSystem");
+            foreach (var o in searcher.Get())
+            {
+                var obj = (ManagementObject)o;
+                totalRamBytes = Convert.ToInt64(obj["TotalPhysicalMemory"]);
+            }
+
+            return (int)BytesToGigabytes(totalRamBytes);
+        }
+
+        public static int GetUsedRam()
+        {
+            long totalRamBytes = 0;
+            var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_ComputerSystem");
+            foreach (var o in searcher.Get())
+            {
+                var obj = (ManagementObject)o;
+                totalRamBytes = Convert.ToInt64(obj["TotalPhysicalMemory"]);
+            }
+
+            long freeRamBytes = 0;
+            searcher = new ManagementObjectSearcher("SELECT * FROM Win32_OperatingSystem");
+            foreach (var o in searcher.Get())
+            {
+                var obj = (ManagementObject)o;
+                freeRamBytes = Convert.ToInt64(obj["FreePhysicalMemory"]) * 1024;
+            }
+
+            var usedRamBytes = totalRamBytes - freeRamBytes;
+
+            return (int)BytesToGigabytes(usedRamBytes);
+        }
+
+        private static double BytesToGigabytes(long bytes)
+        {
+            return Math.Round((double)bytes / (1024 * 1024 * 1024), 2);
+        }
+
+        public static int GetUsedRamAsPercentage()
+        {
+            long totalRam = 0;
+            var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_ComputerSystem");
+            foreach (var o in searcher.Get())
+            {
+                var obj = (ManagementObject)o;
+                totalRam = Convert.ToInt64(obj["TotalPhysicalMemory"]);
+            }
+
+            long freeRam = 0;
+            searcher = new ManagementObjectSearcher("SELECT * FROM Win32_OperatingSystem");
+            foreach (var o in searcher.Get())
+            {
+                var obj = (ManagementObject)o;
+                freeRam = Convert.ToInt64(obj["FreePhysicalMemory"]) * 1024;
+            }
+
+            var usedRam = totalRam - freeRam;
+            var usedRamPercentage = (float)usedRam / totalRam * 100;
+
+            return (int)usedRamPercentage;
+        }
+    }
+}
