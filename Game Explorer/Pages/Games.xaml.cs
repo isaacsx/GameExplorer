3b1b251b4e2e013af731cd000ba102b5f980694d
isaac <isaacramosflores454@gmail.com>
2024-07-25 14:56:27 -0600

    feat: Added some pages

---
+++ Game Explorer/Pages/Games.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using Game_Explorer.Class;
+using Game_Explorer.Components;
+
+namespace Game_Explorer.Pages
+{
+    /// <summary>
+    /// Interaction logic for Games.xaml
+    /// </summary>
+    public sealed partial class Games
+    {
+        public Games()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadGames()
+        {
+            GamesContainer.Children.Clear();
+
+            var installedGames = GameDetector.DetectInstalledGamesWithLogos();
+            foreach (var game in installedGames)
+            {
+                GamesContainer.Children.Add(new Game(game) { Width = 160, Height = 100, Margin = new Thickness(5) });
+            }
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadGames();
+        }
+
+        private void ReloadGames_Click(object sender, RoutedEventArgs e)
+        {
+            LoadGames();
+        }
+    }
+}
---
+++ Game Explorer/Pages/Home.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Game_Explorer.Pages
+{
+    /// <summary>
+    /// Interaction logic for Home.xaml
+    /// </summary>
+    public partial class Home
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+    }
+}
